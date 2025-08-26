4e1fa51b9a8fc8ee5ec962aa690715ed5886aa14
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-08-26 23:19:11 +0900

    init: Create products-basic project

---
+++ apps/2025-08-25-products-basic/src/test/java/com/example/productsbasic/ProductsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
