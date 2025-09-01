27d5027545b34e07f7c1c41fc3efad7c33fac99a
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-09-01 23:50:10 +0900

    feat: ControllerのGETメソッドとPOSTメソッドを作成

---
+++ apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.productsbasic;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductRepository repo;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = repo.findAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = repo.findById(id).orElseThrow();
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        Product createProduct = repo.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createProduct);
+    }
+}
