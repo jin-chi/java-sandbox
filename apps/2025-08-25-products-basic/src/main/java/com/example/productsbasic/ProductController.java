077a4e582fb84acf80867a8dfc6e8ddf536a14ea
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-09-06 18:08:20 +0900

    feat: DELETEメソッドの処理を追加

--- apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/ProductController.java
+++ apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/ProductController.java
@@ -1,9 +1,11 @@
 package com.example.productsbasic;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +51,15 @@ public class ProductController {
         updateProduct.setStock(product.getStock());
         repo.save(updateProduct);
         return ResponseEntity.ok(updateProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@Valid @PathVariable Long id) {
+        Optional<Product> productOptional = repo.findById(id);
+        if (productOptional.isPresent()) {
+            repo.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
