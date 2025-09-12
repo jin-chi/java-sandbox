fa8b720421abf4396df27222b60b1c0145151f66
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-09-12 22:03:16 +0900

    fix: フィールドの追加

--- apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/MyProblemDetail.java
+++ apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/MyProblemDetail.java
@@ -2,18 +2,26 @@ package com.example.productsbasic;
 
 import java.net.URI;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.lang.Nullable;
 
 public class MyProblemDetail extends ProblemDetail {
-    private final Instant timestamp;
 
-    public MyProblemDetail(HttpStatus status, String detail, URI type) {
+    private final Instant timestamp;
+    @Nullable
+    private List<Map<String, String>> errors;
+
+    public MyProblemDetail(HttpStatus status, String detail, URI type, URI instance) {
         super();
         this.setStatus(status.value());
         this.setDetail(detail);
         this.setType(type);
+        this.setInstance(instance);
         this.timestamp = Instant.now();
     }
 
@@ -21,7 +29,16 @@ public class MyProblemDetail extends ProblemDetail {
         return this.timestamp;
     }
 
-    public static MyProblemDetail forStatusAndDetailAndType(HttpStatus status, String detail, URI type) {
-        return new MyProblemDetail(status, detail, type);
+    public List<Map<String, String>> getErrors() {
+        return this.errors;
+    }
+
+    public void setErrors(List<Map<String, String>> errors) {
+        this.errors = new ArrayList<>(errors);
+    }
+
+    public static MyProblemDetail forStatusAndDetailAndType(HttpStatus status, String detail, URI type,
+            URI instance) {
+        return new MyProblemDetail(status, detail, type, instance);
     }
 }
