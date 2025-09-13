d0ce799067130ca1542484fdda4367b51334872e
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-09-13 20:26:58 +0900

    feat: DTO 変換メソッド追加

---
+++ apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/entity/Product.java
@@ -0,0 +1,95 @@
+package com.example.productsbasic.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.example.productsbasic.dto.ProductRequestDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // parameterrized constructor
+    public Product(String name, BigDecimal price, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // default constructor
+    public Product() {
+    }
+
+    @PrePersist
+    public void onCreated() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public static Product from(ProductRequestDto req) {
+        return new Product(req.name(), req.price(), req.stock());
+    }
+}
