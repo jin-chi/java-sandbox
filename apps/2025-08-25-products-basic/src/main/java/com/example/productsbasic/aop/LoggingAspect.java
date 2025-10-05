117d606800acabb0d8a33b26712d07b94fe87f24
jin-chi <87703099+jin-chi@users.noreply.github.com>
2025-10-05 18:21:32 +0900

    fix: ログ管理用Aspectの改善

---
+++ apps/2025-08-25-products-basic/src/main/java/com/example/productsbasic/aop/LoggingAspect.java
@@ -0,0 +1,50 @@
+package com.example.productsbasic.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.example.productsbasic.controller.*.*(..)) || execution(* com.example.productsbasic.service.*.*(..))")
+    public void apiPerformanceTargets() {
+    }
+
+    @Around("apiPerformanceTargets()")
+    public Object logServiceCompletion(ProceedingJoinPoint pjp) throws Throwable {
+        final long startTime = System.currentTimeMillis();
+        String methodName = pjp.getSignature().toShortString();
+        String className = pjp.getSignature().getDeclaringTypeName();
+
+        logger.info("[処理開始]: class={}, method={}", className, methodName);
+
+        Object result = null;
+        Throwable throwable = null;
+
+        try {
+            result = pjp.proceed();
+            return result;
+
+        } catch (Throwable t) {
+            throwable = t;
+            throw t;
+
+        } finally {
+            final long duration = System.currentTimeMillis() - startTime;
+            if (throwable == null) {
+                logger.info("[処理終了]: status=ok, class={}, method={}, processTime={}", className, methodName, duration);
+            } else {
+                logger.error("[処理終了]: status=error, class={}, method={}, exType={}, msg={}, processTime={}", className,
+                        methodName, throwable.getClass().getSimpleName(), throwable.getMessage(), duration);
+            }
+        }
+    }
+}
