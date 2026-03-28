7765c24544a7cfa829f7c8fad2403380c8ed9203
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-03-28 12:14:47 +0900

    First commit

---
+++ apps/spring-jpa-query-annotation-practice/src/test/java/com/example/spring_jpa_query_annotation_practice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_query_annotation_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
