e19c6da42051cc67d90435f02045273c7325a915
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-05-31 12:05:48 +0900

    update

--- apps/2026-03-28-springjpa-spec/src/main/java/com/example/springjpa_spec/UserController.java
+++ apps/2026-03-28-springjpa-spec/src/main/java/com/example/springjpa_spec/UserController.java
@@ -4,18 +4,23 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-  
-    // UserServiceをDIする
-    UserService userService;
 
-    // GETエンドポイント。name, ageは任意のクエリパラメータ
-    @GetMapping("/")
-    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer age) {
         return ResponseEntity.ok(userService.search(name, age));
 
     }
--- apps/2026-03-28-springjpa-spec/src/main/java/com/example/springjpa_spec/UserSpecifications.java
+++ apps/2026-03-28-springjpa-spec/src/main/java/com/example/springjpa_spec/UserSpecifications.java
@@ -5,16 +5,12 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecifications {
 
     public static Specification<User> hasName(String name) {
-        if (name == null || name.isEmpty()) {
-            return null;
-        }
+        if (name == null || name.isEmpty())  return null;
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name);
     }
 
     public static Specification<User> hasAge(Integer age) {
-        if (age == null) {
-            return null;
-        }
+        if (age == null)  return null;
         return (root, query, criteriaBulder) -> criteriaBulder.equal(root.get("age"), age);
     }
 }
