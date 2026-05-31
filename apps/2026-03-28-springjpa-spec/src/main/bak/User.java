51b48a2d144933995aecd786654d984c4fcb3d79
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-05-31 13:02:45 +0900

    Phase1 Step3 Impl

--- apps/2026-03-28-springjpa-spec/src/main/bak/User.java
+++ apps/2026-03-28-springjpa-spec/src/main/bak/User.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "users")
+@Entity
+@Table(name = "users")
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
@@ -19,5 +23,6 @@ public class User {
     private Long id;
 
     private String name;
+
     private Integer age;
 }
--- apps/2026-03-28-springjpa-spec/src/main/bak/UserService.java
+++ apps/2026-03-28-springjpa-spec/src/main/bak/UserService.java
@@ -14,8 +14,9 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getUsers(String name, Integer age) {
+    public List<User> search(String name, Integer age) {
         if ((name == null || name.isEmpty()) && age == null) return List.of();
+
         Specification<User> spec = Specification
                                         .where(UserSpecifications.hasName(name))
                                         .and(UserSpecifications.hasAge(age));
