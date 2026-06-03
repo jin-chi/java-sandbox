128c8b913822f787b173d2cca643725b3e35a537
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-06-03 20:59:54 +0900

    Phase2 Step2 Impl

--- apps/2026-03-28-springjpa-spec/src/test/java/com/example/springjpa_spec/UserServiceTest.java
+++ apps/2026-03-28-springjpa-spec/src/test/java/com/example/springjpa_spec/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.springjpa_spec;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -34,7 +35,19 @@ class UserServiceTest {
     // nameのみ指定のとき、findAllを呼び結果を返す
     @Test
     void search_callsFindAll_whenOnlyNameIsGiven() {
+        List<User> users = new ArrayList<>();
+        User mockUser = User.builder()
+                            .name("taro")
+                            .build();
+        users.add(mockUser);
+        when(userRepo.findAll(ArgumentMatchers.<Specification<User>>any())).thenReturn(users);
 
+        List<User> result = userService.search("taro", null);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getName()).isEqualTo("taro");
+        assertThat(result.get(0).getAge()).isEqualTo(null);
+        verify(userRepo, times(1)).findAll(ArgumentMatchers.<Specification<User>>any());
     }
 
     // argのみ指定のとき、findAllを呼び結果を返す
