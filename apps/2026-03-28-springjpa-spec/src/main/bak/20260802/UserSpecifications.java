b6b810442a1db13b491c94e4fc1fbd99720d1162
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-08-03 02:39:15 +0900

    update

--- apps/2026-03-28-springjpa-spec/src/main/bak/20260802/UserSpecifications.java
+++ apps/2026-03-28-springjpa-spec/src/main/bak/20260802/UserSpecifications.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecifications {
 
     public static Specification<User> hasName(String name) {
-        return (root, query, criteriaBuilder) -> (name == null || name.isEmpty()) ? criteriaBuilder.conjunction()
+        return (root, query, criteriaBuilder) -> (name == null || name.isEmpty()) ? null
                 : criteriaBuilder.equal(root.get("name"), name);
     }
 
     public static Specification<User> hasAge(Integer age) {
-        return (root, query, criteriaBuild) -> age == null ? criteriaBuild.conjunction() : criteriaBuild.equal(root.get("age"), age);
+        return (root, query, criteriaBuilder) -> (age == null) ? null : criteriaBuilder.equal(root.get("age"), age);
     }
 }
