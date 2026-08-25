35f84a725f04448dbe369b51641e243cc481f126
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-08-25 16:18:28 +0900

    結合方法を明示的に記載。LEFT JOINに統一させる

--- apps/2026-08-16-order-search/src/main/java/com/example/order_search/repository/OrderSpecifications.java
+++ apps/2026-08-16-order-search/src/main/java/com/example/order_search/repository/OrderSpecifications.java
@@ -11,6 +11,8 @@ import com.example.order_search.entity.CustomerRank;
 import com.example.order_search.entity.Order;
 import com.example.order_search.entity.OrderStatus;
 
+import jakarta.persistence.criteria.JoinType;
+
 public class OrderSpecifications {
 
     public static Specification<Order> orderNumberContains(String orderNumber) {
@@ -58,7 +60,7 @@ public class OrderSpecifications {
 
     public static Specification<Order> customerRankEquals(CustomerRank customerRank) {
         return (root, query, cb) -> customerRank != null
-                ? cb.equal(root.get("customer").get("rank"), customerRank)
+                ? cb.equal(root.join("customer", JoinType.LEFT).get("rank"), customerRank)
                 : cb.conjunction();
     }
 }
