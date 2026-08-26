7b99fcd6d69b11c3bc607dc99fccf99221d83f1b
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-08-27 01:05:07 +0900

    Project init: Transaction search

---
+++ apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/TransactionSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSearchApplication.class, args);
+	}
+
+}
---
+++ apps/2026-08-27-transaction-search/src/test/java/com/example/transaction_search/TransactionSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transaction_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
