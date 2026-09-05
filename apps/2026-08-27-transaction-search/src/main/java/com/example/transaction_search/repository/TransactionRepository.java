dce62cac0124fce4637a0303c756b6cae33fefc9
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-09-06 02:53:18 +0900

    add EntityGraph to findAll

--- apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/repository/TransactionRepository.java
+++ apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/repository/TransactionRepository.java
@@ -1,5 +1,9 @@
 package com.example.transaction_search.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,4 +11,6 @@ import com.example.transaction_search.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
 
+    @EntityGraph(attributePaths = "account")
+    public Page<Transaction> findAll(Specification<Transaction> spec, Pageable pageable);
 }
