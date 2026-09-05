c20144ed68fe395f4a57913f0781067bc4166c61
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-09-06 02:10:35 +0900

    add request dto

---
+++ apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/dto/TransactionRequestDto.java
@@ -0,0 +1,43 @@
+package com.example.transaction_search.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.example.transaction_search.entity.AccountTier;
+import com.example.transaction_search.entity.TransactionType;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TransactionRequestDto {
+
+    @Size(min = 1, max = 100, message = "1文字以上100文字以下で指定してください")
+    private String transactionRef;
+
+    @Size(min = 1, max = 4, message = "1つ以上4つ以下で指定してください")
+    private List<TransactionType> types;
+
+    @Min(value = 0, message = "0以上の値を指定してください")
+    private Long amountFrom;
+
+    @Min(value = 0, message = "0以上の値を指定してください")
+    private Long amountTo;
+
+    private LocalDate executedFrom;
+    private LocalDate executedTo;
+
+    @Size(min = 1, max = 100, message = "1文字以上100文字以下で指定してください")
+    private String ownerName;
+
+    private AccountTier accountTier;
+    private Boolean unreviewed;
+    
+    @Size(min = 1, max = 100, message = "1文字以上100文字以下で指定してください")
+    private String keyword;
+
+    private Boolean hasFaildTransaction;
+}
