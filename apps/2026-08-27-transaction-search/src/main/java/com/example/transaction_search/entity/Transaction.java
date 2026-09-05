5b8972e04f1305c4aa4276785664dcdba4d39410
jin-chi <87703099+jin-chi@users.noreply.github.com>
2026-09-06 02:09:43 +0900

    アノテーション追加

--- apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/entity/Transaction.java
+++ apps/2026-08-27-transaction-search/src/main/java/com/example/transaction_search/entity/Transaction.java
@@ -2,10 +2,15 @@ package com.example.transaction_search.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,11 +27,26 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String transactionRef;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
     private Account account;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionStatus stauts;
+
+    @Column(nullable = false)
     private Long amount;
+
+    @Column(nullable = false)
     private LocalDateTime executedAt;
+
     private String reviewedBy;
 }
